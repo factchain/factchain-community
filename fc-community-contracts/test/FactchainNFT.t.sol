0644fb123c0d57a7095c1a9a1ac55c022c9d5e1d
Pierre Hay <hay.pierre.pro@gmail.com>
2024-01-17 17:33:43 +0100

    [contracts] fix test

--- fc-community-contracts/test/FactchainNFT.t.sol
+++ fc-community-contracts/test/FactchainNFT.t.sol
@@ -7,19 +7,21 @@ import {FactchainNFT} from "../src/FactchainNFT.sol";
 contract FactchainNFTTest is Test {
     FactchainNFT collection;
 
+contract FactChainNFTTest is Test {
+    FactChainNFT collection;
+
     address public owner = address(1);
     address public recipient = address(2);
     address public factChainSFT = address(3);
     address[] public raters = [address(4), address(5)];
-    string public postUrl = "https://twitter.com/rektorship/status/1750724519705153835";
 
     function setUp() public {
-        collection = new FactchainNFT(owner, factChainSFT, "https://gateway.pinata.cloud/ipfs/");
+        collection = new FactChainNFT(owner, factChainSFT, "https://gateway.pinata.cloud/ipfs/");
     }
 
     function testGetTokenURI() public {
         vm.prank(owner);
-        collection.mint(recipient, postUrl, raters, "QmdByzCXRCbPZ2DYq3wqURjShNEGZ2qDNW4w9mcwA9oWLv");
+        collection.mint(recipient, raters, "QmdByzCXRCbPZ2DYq3wqURjShNEGZ2qDNW4w9mcwA9oWLv");
         assertEq(
             collection.tokenURI(1), "https://gateway.pinata.cloud/ipfs/QmdByzCXRCbPZ2DYq3wqURjShNEGZ2qDNW4w9mcwA9oWLv"
         );
@@ -28,7 +30,7 @@ contract FactchainNFTTest is Test {
     function testSetBaseURI() public {
         vm.startPrank(owner);
         collection.setBaseURI("http://ipfs.com/");
-        collection.mint(recipient, postUrl, raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt");
+        collection.mint(recipient, raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt");
         assertEq(collection.tokenURI(1), "http://ipfs.com/QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt");
     }
 
--- fc-community-contracts/test/FactchainSFT.t.sol
+++ fc-community-contracts/test/FactchainSFT.t.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 
-import {FactchainSFT, IFactchainSFT} from "../src/FactchainSFT.sol";
+import {FactChainSFT, IFactChainSFT} from "../src/FactChainSFT.sol";
 
-contract FactchainSFTTest is Test, IFactchainSFT {
-    FactchainSFT collection;
+contract FactChainSFTTest is Test, IFactChainSFT {
+    FactChainSFT collection;
 
     uint256 public constant MINT_PRICE = 1_000_000;
 
     address public owner = address(1);
-    address public factchainNFT = address(2);
+    address public factchainMain = address(2);
     address public rater1 = address(3);
     address public rater2 = address(4);
     address public rater3 = address(5);
@@ -20,42 +20,38 @@ contract FactchainSFTTest is Test, IFactchainSFT {
     address[] public raters = [rater1, rater2, rater3];
 
     function setUp() public {
-        collection = new FactchainSFT(owner);
+        collection = new FactChainSFT(owner, factchainMain);
         vm.deal(nftBuyer, 100 ether);
         vm.deal(creator, 100 ether);
-        vm.deal(factchainNFT, 100 ether);
+        vm.deal(factchainMain, 100 ether);
     }
 
     function testCloseMint() public {
-        vm.startPrank(owner);
-        collection.setFactchainNFTContract(factchainNFT);
         vm.startPrank(address(100));
-        vm.expectRevert(IFactchainSFT.ReservedToFactchain.selector);
-        collection.initialMint(creator, raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
+        vm.expectRevert(IFactChainSFT.ReservedToFactChain.selector);
+        collection.mint(raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
         assertEq(collection.uri(42), "https://gateway.pinata.cloud/ipfs/");
-        vm.startPrank(factchainNFT);
-        collection.initialMint(creator, raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
+        vm.startPrank(factchainMain);
+        collection.mint(raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
         assertEq(collection.uri(42), "https://gateway.pinata.cloud/ipfs/QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt");
     }
 
     function testOpenMint() public {
-        vm.startPrank(owner);
-        collection.setFactchainNFTContract(factchainNFT);
         vm.startPrank(nftBuyer);
-        vm.expectRevert(IFactchainSFT.BadMintPrice.selector);
-        collection.mint{value: MINT_PRICE + 1}(42, 1);
-        vm.expectRevert(IFactchainSFT.SupplyExhausted.selector);
-        collection.mint{value: MINT_PRICE * 43}(42, 43);
+        vm.expectRevert(IFactChainSFT.BadMintPrice.selector);
+        collection.mint{value: MINT_PRICE + 1}(42, 1, creator);
+        vm.expectRevert(IFactChainSFT.SupplyExhausted.selector);
+        collection.mint{value: MINT_PRICE * 43}(42, 43, creator);
         // first valid mint should be initiated by the factchain main contract
         // to rewards all raters of the note, raise SupplyExhaused otherwise
-        vm.startPrank(factchainNFT);
-        collection.initialMint(creator, raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
+        vm.startPrank(factchainMain);
+        collection.mint(raters, "QmNSYBE2J3gnDrwXJAVnL6KfCzKvsgnWFVpxzyUUWcDmtt", 42);
         uint256 creatorBalanceBefore = address(creator).balance;
         uint256 factchainSFTBalanceBefore = address(collection).balance;
         vm.expectEmit();
         emit FactchainBuildersRewarded(MINT_PRICE / 2);
         emit CreatorRewarded(creator, MINT_PRICE / 2);
-        collection.mint{value: MINT_PRICE}(42, 1);
+        collection.mint{value: MINT_PRICE}(42, 1, creator);
         assertEq(
             address(creator).balance,
             creatorBalanceBefore + MINT_PRICE / 2,
@@ -67,8 +63,8 @@ contract FactchainSFTTest is Test, IFactchainSFT {
             "Factchain builders should have been rewarded too by half MintPrice"
         );
         // should exhaust the supply
-        collection.mint{value: MINT_PRICE * 41}(42, 41);
-        vm.expectRevert(IFactchainSFT.SupplyExhausted.selector);
-        collection.mint{value: MINT_PRICE}(42, 1);
+        collection.mint{value: MINT_PRICE * 41}(42, 41, creator);
+        vm.expectRevert(IFactChainSFT.SupplyExhausted.selector);
+        collection.mint{value: MINT_PRICE}(42, 1, creator);
     }
 }
