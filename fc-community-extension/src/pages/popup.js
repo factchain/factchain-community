42acad8728d5ffc4fd956cc0d8e0572a20db9d48
Yacine Badiss <yacine.badiss@gmail.com>
2024-03-07 12:00:10 +0100

    [extension] fix prettier

--- fc-community-extension/src/pages/popup.js
+++ fc-community-extension/src/pages/popup.js
@@ -59,7 +59,10 @@ function FCProfile(props) {
           <div className="flex items-center justify-between rounded-2xl px-10 py-6">
             <StatCard name="Notes" value={props.numberNotes} />
             <StatCard name="Ratings" value={props.numberRatings} />
-            <StatCard name="Earnings" value={`${props.earnings.slice(0, 7)} ⧫`} />
+            <StatCard
+              name="Earnings"
+              value={`${props.earnings.slice(0, 7)} ⧫`}
+            />
           </div>
           {props.loggedIn && <FCNetworks />}
         </div>
